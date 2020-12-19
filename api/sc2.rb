25e8cacf6ae1ca8bd9036ca5761c91c2af4a645a
alexchaoss <45114914+alexchaoss@users.noreply.github.com>
2020-12-19 00:04:58 -0500

    Update sc2.rb

--- api/sc2.rb
+++ api/sc2.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'blizzard_api'
 require 'sinatra'
 
@@ -57,4 +59,36 @@ end
 get '/sc2/player/:account_id' do |account_id|
   @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
   sc2_api_client.account(@region).player(account_id, @options).to_json
+end
+
+# Legacy API
+
+get '/sc2/legacy/profile/:region_id/:realm_id/:profile_id' do |region_id, realm_id, profile_id|
+  @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
+  sc2_api_client.legacy(@region).profile(region_id.to_sym, realm_id, profile_id, @options).to_json
+end
+
+get '/sc2/legacy/profile/:region_id/:realm_id/:profile_id/ladder' do |region_id, realm_id, profile_id|
+  @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
+  sc2_api_client.legacy(@region).ladders(region_id.to_sym, realm_id, profile_id, @options).to_json
+end
+
+get '/sc2/legacy/profile/:region_id/:realm_id/:profile_id/matches' do |region_id, realm_id, profile_id|
+  @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
+  sc2_api_client.legacy(@region).match(region_id.to_sym, realm_id, profile_id, @options).to_json
+end
+
+get '/sc2/legacy/ladder/:region_id/:ladder_id' do |region_id, ladder_id|
+  @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
+  sc2_api_client.legacy(@region).ladder(region_id.to_sym, ladder_id, @options).to_json
+end
+
+get '/sc2/legacy/data/achievements/:region_id' do |region_id|
+  @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
+  sc2_api_client.legacy(@region).achievements(region_id.to_sym, @options).to_json
+end
+
+get '/sc2/legacy/data/rewards/:region_id' do |region_id|
+  @options[:token] = TokenMap.instance.get(params[:token]) if params[:token]
+  sc2_api_client.legacy(@region).rewards(region_id.to_sym, @options).to_json
 end
