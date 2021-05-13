58ad1d02f2c5a728c18a4ab4fa9b57cd511585dc
alexchaoss <45114914+alexchaoss@users.noreply.github.com>
2021-05-13 08:03:21 -0400

    Revert "Revert 7 master"

--- api/hs.rb
+++ api/hs.rb
@@ -13,36 +13,36 @@ hs_api_client = BlizzardApi::Hearthstone
 
 get '/hearthstone/cards' do
   search_options = params.select { |key| BlizzardApi::Hearthstone::Card::VALID_SEARCH_OPTIONS.include? key.to_sym }
-  hs_api_client.card(@region).search(search_options, @options).to_json
+  hs_api_client.card(@region).search(search_options, **@options)&.to_json
 end
 
 get '/hearthstone/cards/:slug' do |slug|
-  hs_api_client.card(@region).get(slug, 'constructed', @options).to_json
+  hs_api_client.card(@region).get(slug, 'constructed', **@options)&.to_json
 end
 
 # Card backs
 
 get '/hearthstone/cardbacks' do
   search_options = params.select { |key| BlizzardApi::Hearthstone::Back::VALID_SEARCH_OPTIONS.include? key.to_sym }
-  hs_api_client.back(@region).search(search_options, @options).to_json
+  hs_api_client.back(@region).search(search_options, **@options)&.to_json
 end
 
 get '/hearthstone/cards/:slug' do |slug|
-  hs_api_client.back(@region).get(slug, @options).to_json
+  hs_api_client.back(@region).get(slug, **@options)&.to_json
 end
 
 # Deck
 
 get '/hearthstone/deck/:slug' do |slug|
-  hs_api_client.deck(@region).get(slug, @options).to_json
+  hs_api_client.deck(@region).get(slug, **@options)&.to_json
 end
 
 # Metadata
 
 get '/hearthstone/metadata' do
-  hs_api_client.metadata(@region).index(@options).to_json
+  hs_api_client.metadata(@region).index(**@options)&.to_json
 end
 
 get '/hearthstone/metadata/:type' do |type|
-  hs_api_client.metadata(@region).get(type, @options).to_json
+  hs_api_client.metadata(@region).get(type, **@options)&.to_json
 end
